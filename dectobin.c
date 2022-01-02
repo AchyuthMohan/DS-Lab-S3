c69063e1e480cc35fa79992eab101611e4db6167
achyut <75477017+AchyuthMohan@users.noreply.github.com>
2022-01-02 09:40:20 +0530

    spacing updated

--- dectobin.c
+++ dectobin.c
@@ -1,27 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define MAX 50
-
 int isEmpty(int top, int stack_arr[]);
 void push(int x, int *top, int stack_arr[]);
 int pop(int *top, int stack_arr[]);
 void DecToBin(int num);
 void DectoHex(int num);
-
 int main()
-{
-        int num;
+{       int num;
         printf("Enter an integer : ");
         scanf("%d",&num);
         printf("Binary Equivalent is : ");
         DecToBin(num);
         printf("\nHexadecimal Equivalent is : ");
         DectoHex(num);
-
         return 0;
-
 }
-
 void DecToBin(int num)
 {
         int stack[MAX], top=-1, rem;
@@ -48,8 +42,6 @@ void DectoHex(int num)
                 printf("%d", pop(&top, stack));
         printf("\n");
 }
-
-
 void push(int x, int *top, int stack_arr[])
 {
         if(*top == (MAX-1))
@@ -60,7 +52,6 @@ void push(int x, int *top, int stack_arr[])
                 stack_arr[*top] = x;
         }
 }
-
 int pop(int *top, int stack_arr[])
 {
         int x;
